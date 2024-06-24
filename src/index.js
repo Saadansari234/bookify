5e25d55ea6a6f235c8dac869b3829d908937e509
saadansari234 <saadansari3212@gmail.com>
2024-06-24 18:44:22 +0530

    hh

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App.js'
 import { FirebaseProvider } from './context/Firebase.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
-
+// mddmdm
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <FirebaseProvider>
